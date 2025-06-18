e8b074bdaf0b3aa8ec99fd626e280290dbc9e44b
PauAdjemian <paulaadjemian2004@gmail.com>
2025-06-18 18:37:42 -0300

    agregar ruta

--- obligatorio/src/app/app.routes.ts
+++ obligatorio/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LogInIntegranteMesa } from './log-in-integrante-mesa/log-in-integrante-mesa'; 
 import { LogInAdmin } from './log-in-admin/log-in-admin';
+import { LogInVotante } from './log-in-votante/log-in-votante';
 
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
 {"path": '', component: LogInIntegranteMesa},
 {"path": 'admin', component: LogInAdmin},
+{"path": 'votante', component: LogInVotante},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
