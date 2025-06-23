33130a50340d86e7a86e75edd552e64ecee74300
PauAdjemian <paulaadjemian2004@gmail.com>
2025-06-23 08:17:28 -0300

    pop up agregar establecimiento

--- obligatorio/src/app/tareas-admin/tareas-admin.ts
+++ obligatorio/src/app/tareas-admin/tareas-admin.ts
@@ -2,16 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AgregarEleccion } from "../agregar-eleccion/agregar-eleccion";
+import { AgregarEstablecimiento } from "../agregar-establecimiento/agregar-establecimiento";
 
 @Component({
   selector: 'app-tareas-admin',
-  imports: [CommonModule, FormsModule, AgregarEleccion],
+  imports: [CommonModule, FormsModule, AgregarEleccion, AgregarEstablecimiento],
   templateUrl: './tareas-admin.html',
   styleUrl: './tareas-admin.css'
 })
 export class TareasAdmin {
 
   @ViewChild('PopupEleccion') popupEleccion!: AgregarEleccion;
+  @ViewChild('PopupEstablecimiento') popupEstablecimiento!: AgregarEstablecimiento;
   
   opcionSeleccionada: string = '';
 
@@ -21,6 +23,9 @@ export class TareasAdmin {
     switch (this.opcionSeleccionada) {
       case 'eleccion':
         this.popupEleccion.open();
+        break;
+      case 'establecimiento':
+        this.popupEstablecimiento.open();
         break;
     }
   }
